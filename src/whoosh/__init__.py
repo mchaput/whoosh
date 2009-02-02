690a81cc6b5b1de670bda994d24377dcd325c782
Matt Chaput <matt@whoosh.ca>
2009-02-02 22:44:57 +0000

    Missed a reference to create_index_in when it was renamed to create_in.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,4 +14,4 @@
 # limitations under the License.
 #===============================================================================
 
-from whoosh.index import open_dir, create_index_in
+from whoosh.index import open_dir, create_in
