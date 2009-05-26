14a60072704404a805437a15fa316a1639864575
Matt Chaput <matt@whoosh.ca>
2009-05-26 23:29:00 +0000

    Bumped revision.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "Whoosh",
-	version = "0.1.20",
+	version = "0.1.21",
 	package_dir = {'': 'src'},
 	packages = ["whoosh", "whoosh.lang", "whoosh.support"],
 	
