c543f7dedb3a00da0a162f086b8b93f0608b3ae1
Matt Chaput <matt@whoosh.ca>
2009-08-29 04:12:36 +0000

    Bumped beta tag.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (0, 3, 0, 'b', 8)
+__version__ = (0, 3, 0, 'b', 9)
 
 
 def versionstring(build=True, extra=True):
