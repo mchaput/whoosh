404e99041c8484525efa21c8cd39295e62f3347a
Matt Chaput <matt@whoosh.ca>
2009-11-10 20:52:26 +0000

    Bumped revision number.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (0, 3, 2)
+__version__ = (0, 3, 3)
 
 
 def versionstring(build=True, extra=True):
