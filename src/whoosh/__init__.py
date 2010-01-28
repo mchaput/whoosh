86b555f4423081c8718c2928dd17a23911705bf6
Matt Chaput <matt@whoosh.ca>
2010-01-28 22:07:29 +0000

    Fixed NullFragmenter so it encompasses the entire input text, even if the text starts or ends with non-token characters. Bumped revision number.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (0, 3, 12)
+__version__ = (0, 3, 13)
 
 
 def versionstring(build=True, extra=True):
