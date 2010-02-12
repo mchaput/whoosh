d9cac26f7fb0dbd9684b1b3f7fd999744b924702
mchaput <none@none>
2010-02-11 19:36:02 -0500

    Rewrote README.txt and changed setup.py to get the long description from it.

--- setup.py
+++ setup.py
@@ -16,23 +16,7 @@ setup(
 	author_email = "matt@whoosh.ca",
 	description = "Fast, pure-Python full text indexing, search, and spell checking library.",
 	
-    long_description = """
-Whoosh is a fast, pure-Python indexing and search library. Programmers
-can use it to easily add search functionality to their applications and
-websites. Because Whoosh is pure Python, you don't have to compile or
-install a binary support library and/or make Python work with a JVM, yet
-Whoosh is still very fast at indexing and searching. Every part of how
-Whoosh works can be extended or replaced to meet your needs exactly.
-
-This software is licensed under the terms of the Apache License version 2.
-See LICENSE.txt for information.
-
-The primary source of information is the main Whoosh web site:
-http://whoosh.ca/
-
-You can check out the latest version of the source code from subversion at:
-http://svn.whoosh.ca/projects/whoosh/trunk/
-""",
+    long_description = open("README.txt").read(),
 
 	license = "Apache 2.0",
 	keywords = "index search text spell",
