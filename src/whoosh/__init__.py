6e33096c323826b5882cbdbb564778175bab0dd9
Matt Chaput <matt@whoosh.ca>
2010-10-13 16:40:39 -0400

    Bumped version and changed index format to account for changes due to index compression work.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (1, 0, 1)
+__version__ = (1, 1, 0)
 
 
 def versionstring(build=True, extra=True):
