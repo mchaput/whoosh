2d5de42ce97e26c27f2acdad6dbec0859c83531a
Matt Chaput <matt@whoosh.ca>
2011-01-20 01:53:11 -0500

    Bumper version number.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (1, 5, 5)
+__version__ = (1, 5, 6)
 
 
 def versionstring(build=True, extra=True):
