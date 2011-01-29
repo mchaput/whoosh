9f3037ff435b078c6420553091ec50b5fe5a452e
Matt Chaput <matt@whoosh.ca>
2011-01-29 04:51:11 -0500

    Replaced ramdb package with ramindex module with a bit simpler implementation.

---
+++ src/whoosh/ramindex.py
@@ -0,0 +1,317 @@
+#===============================================================================
+# Copyright 2011 Matt Chaput
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#===============================================================================
+
+from collections import defaultdict
+from bisect import bisect_left
+from threading import RLock
+
+from whoosh.fields import UnknownFieldError
+from whoosh.matching import ListMatcher, NullMatcher
+from whoosh.reading import IndexReader, TermNotFound
+from whoosh.writing import IndexWriter
+from whoosh.util import synchronized
+
+
+class RamIndex(IndexReader, IndexWriter):
+    def __init__(self, schema):
+        self.schema = schema
+        self.docnum = 0
+        self._sync_lock = RLock()
+        self.is_closed = False
+        self.clear()
+    
+    @synchronized
+    def clear(self):
+        self.invindex = {}
+        self.indexfreqs = defaultdict(int)
+        self.storedfields = []
+        self.fieldlengths = defaultdict(int)
+        self.vectors = {}
+        self.deleted = set()
+        self.usage = 0
+    
+    @synchronized
+    def __contains__(self, term):
+        try:
+            self.invindex[term[0]][term[1]]
+            return True
+        except KeyError:
+            return False
+    
+    @synchronized
+    def __iter__(self):
+        invindex = self.invindex
+        indexfreqs = self.indexfreqs
+        for fn in sorted(invindex.keys()):
+            for text in sorted(self.invindex[fn].keys()):
+                docfreq = len(invindex[fn][text])
+                indexfreq = indexfreqs[fn, text]
+                yield (fn, text, docfreq, indexfreq)
+    
+    def close(self):
+        pass
+    
+    @synchronized
+    def has_deletions(self):
+        return bool(self.deleted)
+    
+    @synchronized
+    def is_deleted(self, docnum):
+        return docnum in self.deleted
+    
+    @synchronized
+    def delete_document(self, docnum):
+        self.deleted.add(docnum)
+    
+    @synchronized
+    def stored_fields(self, docnum):
+        return self.storedfields[docnum]
+    
+    @synchronized
+    def all_stored_fields(self):
+        deleted = self.deleted
+        return (sf for i, sf in enumerate(self.storedfields)
+                if i not in deleted)
+    
+    @synchronized
+    def field_length(self, fieldname):
+        if fieldname not in self.schema or not self.schema[fieldname].scorable:
+            return 0
+        return sum(l for docnum_fieldname, l in self.fieldlengths.iteritems()
+                   if docnum_fieldname[1] == fieldname)
+    
+    @synchronized
+    def max_field_length(self, fieldname):
+        if fieldname not in self.schema or not self.schema[fieldname].scorable:
+            return 0
+        return max(l for docnum_fieldname, l in self.fieldlengths.iteritems()
+                   if docnum_fieldname[1] == fieldname)
+    
+    def doc_field_length(self, docnum, fieldname, default=0):
+        return self.fieldlengths.get((docnum, fieldname), default)
+    
+    def has_vector(self, docnum, fieldname):
+        return (docnum, fieldname) in self.vectors
+    
+    @synchronized
+    def vector(self, docnum, fieldname):
+        vformat = self.schema[fieldname].vector
+        ids, weights, values = zip(*self.vectors[docnum, fieldname])
+        return ListMatcher(ids, weights, values, format=vformat)
+    
+    def doc_frequency(self, fieldname, text):
+        return len(self.invindex[fieldname][text])
+    
+    def frequency(self, fieldname, text):
+        return self.indexfreqs[fieldname, text]
+    
+    @synchronized
+    def iter_from(self, fieldname, text):
+        invindex = self.invindex
+        indexfreqs = self.indexfreqs
+        
+        for fn in sorted(key for key in self.invindex.keys() if key >= fieldname):
+            texts = sorted(invindex[fn])
+            start = 0
+            if fn == fieldname:
+                start = bisect_left(texts, text)
+            for t in texts[start:]:
+                docfreq = len(invindex[fn][t])
+                indexfreq = indexfreqs[fn, t]
+                yield (fn, t, docfreq, indexfreq)
+    
+    def lexicon(self, fieldname):
+        return sorted(self.invindex[fieldname].keys())
+    
+    @synchronized
+    def expand_prefix(self, fieldname, prefix):
+        texts = self.lexicon(fieldname)
+        start = 0 if not prefix else bisect_left(texts, prefix)
+        for text in texts[start:]:
+            if text.startswith(prefix):
+                yield text
+            else:
+                break
+    
+    @synchronized
+    def first_id(self, fieldname, text):
+        try:
+            plist = self.invindex[fieldname][text]
+        except KeyError:
+            raise TermNotFound((fieldname, text))
+        else:
+            deleted = self.deleted
+            for x in plist:
+                docnum = x[0]
+                if docnum not in deleted:
+                    return docnum
+    
+    @synchronized
+    def postings(self, fieldname, text, scorer=None):
+        try:
+            postings = self.invindex[fieldname][text]
+        except KeyError:
+            raise TermNotFound((fieldname, text))
+        
+        excludeset = self.deleted
+        format = self.schema[fieldname].format
+        if excludeset:
+            postings = [x for x in postings if x[0] not in excludeset]
+            if not postings:
+                return NullMatcher()
+        ids, weights, values = zip(*postings)
+        return ListMatcher(ids, weights, values, format=format)
+    
+    def reader(self):
+        return self
+    
+    def searcher(self, **kwargs):
+        from whoosh.searching import Searcher
+        return Searcher(self.reader(), **kwargs)
+    
+    def writer(self, **kwargs):
+        return self
+    
+    def doc_count_all(self):
+        return len(self.storedfields)
+    
+    def doc_count(self):
+        return len(self.storedfields) - len(self.deleted)
+    
+    @synchronized
+    def update_document(self, **fields):
+        super(RamIndex, self).update_document(**fields)
+    
+    @synchronized
+    def add_document(self, **fields):
+        schema = self.schema
+        invindex = self.invindex
+        indexfreqs = self.indexfreqs
+        fieldlengths = self.fieldlengths
+        usage = 0
+        
+        fieldnames = [name for name in sorted(fields.keys())
+                      if not name.startswith("_")]
+        
+        for name in fieldnames:
+            if name not in schema:
+                raise UnknownFieldError("There is no field named %r" % name)
+            if name not in invindex:
+                invindex[name] = {}
+        
+        storedvalues = {}
+        
+        for name in fieldnames:
+            value = fields.get(name)
+            if value:
+                field = schema[name]
+                
+                fielddict = invindex[name]
+                
+                # If the field is indexed, add the words in the value to the
+                # index
+                if field.indexed:
+                    # Count of all terms in the value
+                    count = 0
+                    # Count of UNIQUE terms in the value
+                    unique = 0
+                    
+                    for w, freq, weight, valuestring in field.index(value):
+                        if w not in fielddict:
+                            fielddict[w] = []
+                        fielddict[w].append((self.docnum, weight, valuestring))
+                        indexfreqs[name, w] += freq
+                        count += freq
+                        unique += 1
+                        
+                        usage += 44 + len(valuestring)
+                    
+                    if field.scorable:
+                        fieldlengths[self.docnum, name] = count
+                        usage += 36
+                    
+            vector = field.vector
+            if vector:
+                vlist = sorted((w, weight, valuestring) for w, freq, weight, valuestring
+                               in vector.word_values(value))
+                self.vectors[self.docnum, name] = vlist
+                usage += 28
+                for x in vlist:
+                    usage += 44 + len(x[2])
+            
+            if field.stored:
+                storedname = "_stored_" + name
+                if storedname in fields:
+                    stored_value = fields[storedname]
+                else :
+                    stored_value = value
+                
+                storedvalues[name] = stored_value
+                usage += 28 + len(name)# + len(stored_value)
+        
+        self.storedfields.append(storedvalues)
+        self.usage += usage
+        self.docnum += 1
+    
+#    @synchronized
+#    def optimize(self):
+#        deleted = self.deleted
+#        
+#        # Remove deleted documents from stored fields
+#        self.storedfields = [sf for i, sf in enumerate(self.storedfields)
+#                             if i not in deleted]
+#        
+#        # Remove deleted documents from inverted index
+#        removedterms = defaultdict(set)
+#        for fn in self.invindex:
+#            termdict = self.invindex[fn]
+#            for text, postlist in termdict.items():
+#                newlist = [x for x in postlist if x[0] not in deleted]
+#                if newlist:
+#                    termdict[text] = newlist
+#                else:
+#                    removedterms[fn].add(text)
+#                    del termdict[text]
+#        
+#        # If terms were removed as a result of document deletion, update
+#        # indexfreqs
+#        for fn, removed in removedterms.iteritems():
+#            for text in removed:
+#                del self.indexfreqs[fn, text]
+#        
+#        # Remove documents from field lengths
+#        fieldlengths = self.fieldlengths
+#        for docnum, fieldname in fieldlengths.keys():
+#            if docnum in deleted:
+#                del fieldlengths[docnum, fieldname]
+#                
+#        # Remove documents from vectors
+#        vectors = self.vectors
+#        for docnum, fieldname in vectors.keys():
+#            if docnum in deleted:
+#                del vectors[docnum, fieldname]
+#        
+#        # Reset deleted list
+#        self.deleted = set()
+        
+    def commit(self):
+        pass
+        
+    
+
+
+
+
--- tests/test_ramindex.py
+++ tests/test_ramindex.py
@@ -1,22 +1,18 @@
 from __future__ import with_statement
 import unittest
 
-import os.path
-
-from whoosh import query
-from whoosh.fields import *
-from whoosh.ramdb.ramindex import RamIndex
+from whoosh import analysis, fields, formats, query
+from whoosh.ramindex import RamIndex
 from whoosh.support.testing import TempIndex
 
 
 class TestRamIndex(unittest.TestCase):
     def make_index(self):
-        ana = StandardAnalyzer(stoplist=None)
-        sc = Schema(id=ID(stored=True),
-                    text=TEXT(analyzer=ana, vector=Frequency(ana)),
-                    subs=NUMERIC(int, stored=True))
+        ana = analysis.StandardAnalyzer(stoplist=None)
+        sc = fields.Schema(id=fields.ID(stored=True),
+                           text=fields.TEXT(analyzer=ana, vector=formats.Frequency(ana)),
+                           subs=fields.NUMERIC(int, stored=True))
         ix = RamIndex(sc)
-        
         ix.add_document(id=u"fieldtype",
                         text=u"The FieldType object supports the following attributes",
                         subs=56)
@@ -40,6 +36,54 @@ class TestRamIndex(unittest.TestCase):
                         subs=58204)
         return ix
     
+    def test_docs_method(self):
+        ix = self.make_index()
+        with ix.searcher() as s:
+            self.assertEqual(s.document(id="vector"), {"id": "vector", "subs": 23})
+            self.assertEqual(s.document(subs=2), {"id": "unique", "subs": 2})
+    
+    def test_searching(self):
+        with self.make_index().searcher() as s:
+            def _runq(q, result, **kwargs):
+                r = s.search(q, scored=False, **kwargs)
+                self.assertEqual([d["id"] for d in r], result)
+            
+            _runq(query.Term("text", u"format"), ["format", "vector"])
+            _runq(query.Term("text", u"the"), ["fieldtype", "format", "vector", "stored", "const"])
+            _runq(query.Prefix("text", u"st"), ["format", "vector", "stored"])
+            _runq(query.Wildcard("id", u"*st*"), ["stored", "const"])
+            _runq(query.TermRange("id", u"c", u"s"), ["fieldtype", "format", "const"])
+            _runq(query.NumericRange("subs", 10, 100), ["fieldtype", "format", "vector", "scorable"])
+            _runq(query.Phrase("text", ["this", "field"]), ["scorable", "stored", "unique"], limit=None)
+            _runq(query.Every(), ["fieldtype", "format", "vector", "scorable", "stored", "unique", "const"])
+            _runq(query.Every("subs"), ["fieldtype", "format", "vector", "scorable", "stored", "unique", "const"])
+    
+    def test_update(self):
+        schema = fields.Schema(id=fields.ID(unique=True, stored=True),
+                               text=fields.ID(stored=True))
+        ix = RamIndex(schema)
+        for word in u"alfa bravo charlie delta".split():
+            ix.update_document(id=word[0], text=word)
+        for word in u"apple burrito cat dollhouse".split():
+            ix.update_document(id=word[0], text=word)
+        
+        self.assertTrue(ix.has_deletions())
+        self.assertEqual(ix.deleted, set([0, 1, 2, 3]))
+        self.assertEqual(ix.doc_count(), 4)
+        self.assertEqual([(d["id"], d["text"]) for d in ix.all_stored_fields()],
+                         [("a", "apple"), ("b", "burrito"), ("c", "cat"), ("d", "dollhouse")])
+    
+    def test_contains(self):
+        ix = self.make_index()
+        totext = ix.schema["subs"].to_text
+        self.assertTrue(("text", u"format") in ix)
+        self.assertTrue(("id", u"fieldtype") in ix)
+        self.assertTrue(("subs", totext(23)) in ix)
+        self.assertFalse(("foo", u"bar") in ix)
+        self.assertFalse(("text", u"repeat") in ix)
+        self.assertFalse(("id", u"galonk") in ix)
+        self.assertFalse(("subs", totext(500)) in ix)
+    
     def test_indexing(self):
         ix = self.make_index()
         with ix.searcher() as s:
@@ -51,9 +95,51 @@ class TestRamIndex(unittest.TestCase):
             self.assertEqual(r[1]["id"], "vector")
             self.assertEqual(r[1]["subs"], 23)
 
+    def test_delete_doc(self):
+        ix = self.make_index()
+        ix.delete_document(2)
+        ix.delete_document(5)
+        with ix.searcher() as s:
+            self.assertEqual([d["id"] for d in s.search(query.Every())],
+                             ["fieldtype", "format", "scorable", "stored", "const"])
+
+    def test_stored(self):
+        r = self.make_index().reader()
+        
+        self.assertEqual(r.stored_fields(2), {"id": "vector", "subs": 23})
+        
+        target = [{"id": "fieldtype", "subs": 56},
+                  {"id": "format", "subs": 100},
+                  {"id": "vector", "subs": 23},
+                  {"id": "scorable", "subs": 34},
+                  {"id": "stored", "subs": 575},
+                  {"id": "unique", "subs": 2},
+                  {"id": "const", "subs": 58204},
+                  ]
+        self.assertEqual(list(r.all_stored_fields()), target)
+
+    def test_field_length(self):
+        r = self.make_index().reader()
+        self.assertEqual(r.field_length("id"), 0)  # Not scorable
+        self.assertEqual(r.max_field_length("id"), 0)
+        self.assertEqual(r.field_length("subs"), 0)
+        self.assertEqual(r.max_field_length("subs"), 0)
+        self.assertEqual(r.field_length("text"), 59)
+        self.assertEqual(r.max_field_length("text"), 11)
+        self.assertEqual(r.doc_field_length(3, "text"), 8)
+        
+        self.assertEqual(r.field_length("text"), 59)
+        self.assertEqual(r.max_field_length("text"), 11)
+        self.assertEqual(r.doc_field_length(3, "text"), 8)
+        
+        self.assertEqual(r.doc_frequency("text", "the"), 5)
+        self.assertEqual(r.frequency("text", "the"), 9)
+
     def test_deleting(self):
         ix = self.make_index()
         ix.delete_by_term("id", u"vector")
+        
+        self.assertTrue(ix.has_deletions())
         
         with ix.searcher() as s:
             q = query.Term("text", "format")
@@ -62,47 +148,8 @@ class TestRamIndex(unittest.TestCase):
             self.assertEqual(r[0]["id"], "format")
             self.assertEqual(r[0]["subs"], 100)
         
-    def test_optimize(self):
-        ix = self.make_index()
-        self.assertEqual(ix.doc_count(), 7)
-        self.assertEqual(ix.doc_count_all(), 7)
-        
-        ix.delete_by_term("id", u"vector")
-        self.assertEqual(ix.doc_count(), 6)
-        self.assertEqual(ix.doc_count_all(), 7)
-        self.assertTrue(ix.has_deletions())
-        
-        ix.optimize()
-        self.assertEqual(ix.doc_count(), 6)
-        self.assertEqual(ix.doc_count_all(), 6)
-        self.assertFalse(ix.has_deletions())
-        self.assertFalse(("text", u"forward") in ix.reader())
-        
-        ix.close()
-        
-    def test_reader(self):
-        ix = self.make_index()
-        r = ix.reader()
-        
-        self.assertTrue(("text", u"constructor") in r)
-        self.assertEqual(r.stored_fields(2), {"id": "vector", "subs": 23})
-        
-        target = [{"id": u"fieldtype", "subs": 56},
-                  {"id": u"format", "subs": 100},
-                  {"id": u"vector", "subs": 23},
-                  {"id": u"scorable", "subs": 34},
-                  {"id": u"stored", "subs": 575},
-                  {"id": u"unique", "subs": 2},
-                  {"id": u"const", "subs": 58204},
-                  ]
-        self.assertEqual(list(r.all_stored_fields()), target)
-        
-        self.assertEqual(r.field_length("text"), 59)
-        self.assertEqual(r.max_field_length("text"), 11)
-        self.assertEqual(r.doc_field_length(3, "text"), 8)
-        
-        self.assertEqual(r.doc_frequency("text", "the"), 5)
-        self.assertEqual(r.frequency("text", "the"), 9)
+    def test_iter(self):
+        r = self.make_index().reader()
         
         everything = [("id", u'const', 1, 1), ("id", u'fieldtype', 1, 1), ("id", u'format', 1, 1),
                       ("id", u'scorable', 1, 1), ("id", u'stored', 1, 1), ("id", u'unique', 1, 1),
@@ -121,7 +168,10 @@ class TestRamIndex(unittest.TestCase):
         
         self.assertEqual([item for item in r if item[0] != 'subs'], everything)
         self.assertEqual(list(r.iter_from("text", u"su")), everything[32:])
-        
+        self.assertEqual(list(r.lexicon("text")), [x[1] for x in everything if x[0] == "text"])
+        self.assertEqual(list(r.iter_field("text")), [x[1:] for x in everything if x[0] == "text"])
+        self.assertEqual(list(r.iter_field("text", "st")), [x[1:] for x in everything if x[0] == "text" and x[1] >= "st"])
+    
     def test_vectors(self):
         ix = self.make_index()
         r = ix.reader()
@@ -133,7 +183,7 @@ class TestRamIndex(unittest.TestCase):
                   (u'storage', 1), (u'the', 2)]
         vec = list(r.vector_as("frequency", 1, "text"))
         self.assertEqual(target, vec)
-        
+    
     def test_todisk(self):
         ix = self.make_index()
 
@@ -141,8 +191,45 @@ class TestRamIndex(unittest.TestCase):
             w = fix.writer()
             w.add_reader(ix.reader())
             w.commit()
-
-
+            
+    def test_threaded(self):
+        from threading import Thread
+        
+        class TWriter(Thread):
+            def __init__(self, ix):
+                Thread.__init__(self)
+                self.ix = ix
+                
+            def run(self):
+                ix = self.ix
+                for i in xrange(1000):
+                    ix.update_document(id=unicode(i), key=u"a")
+        
+        class TReader(Thread):
+            def __init__(self, ix):
+                Thread.__init__(self)
+                self.ix = ix
+                self.go = True
+            
+            def run(self):
+                s = self.ix.searcher()
+                while self.go:
+                    r = s.search(query.Term("key", u"a"))
+                    assert len(r) == 1
+        
+        schema = fields.Schema(id=fields.ID(stored=True),key=fields.ID(unique=True, stored=True))
+        ix = RamIndex(schema)
+        tw = TWriter(ix)
+        tr = TReader(ix)
+        tw.start()
+        tr.start()
+        tw.join()
+        tr.go = False
+        tr.join()
+        
+        self.assertEqual(ix.doc_count(), 1)
+        with ix.searcher() as s:
+            self.assertEqual(len(list(s.documents(key="a"))), 1)
 
         
 if __name__ == '__main__':
