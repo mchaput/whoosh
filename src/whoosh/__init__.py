d8c0d9f77cfdf57c20668d5c9b9690b7a4b2aad7
Matt Chaput <matt@whoosh.ca>
2011-02-02 15:06:32 -0500

    Bumped version number and updated release notes for 1.6.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (1, 5, 9)
+__version__ = (1, 6, 0)
 
 
 def versionstring(build=True, extra=True):
