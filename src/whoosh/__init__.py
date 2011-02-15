e0d380227dfc9b59e67788b90afca0796ca32572
Matt Chaput <matt@whoosh.ca>
2011-02-15 03:49:08 -0500

    Bumped version to 1.7.1 for bugfix release. See issue #108.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (1, 7, 0)
+__version__ = (1, 7, 1)
 
 
 def versionstring(build=True, extra=True):
