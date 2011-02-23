39dcefca3549b4803b5f7982534634b204161e09
Matt Chaput <matt@whoosh.ca>
2011-02-22 19:02:19 -0500

    Bumped version number to 1.7.5 for MultiPool bugfix.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (1, 7, 4)
+__version__ = (1, 7, 5)
 
 
 def versionstring(build=True, extra=True):
