9971576b64eb436f0cdfecbe309a89ea33afddc0
Matt Chaput <matt@whoosh.ca>
2011-03-06 18:19:58 -0500

    Updated release notes, bumped version to 1.7.7.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -14,7 +14,7 @@
 # limitations under the License.
 #===============================================================================
 
-__version__ = (1, 7, 6)
+__version__ = (1, 7, 7)
 
 
 def versionstring(build=True, extra=True):
