b0e8c69a89356d913a0fef82c2826da3ab8e86cb
Matt Chaput <matt@whoosh.ca>
2011-03-31 16:36:36 -0400

    Bumped version to 1.8.1.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -25,7 +25,7 @@
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied, of Matt Chaput.
 
-__version__ = (1, 8, 0)
+__version__ = (1, 8, 1)
 
 
 def versionstring(build=True, extra=True):
