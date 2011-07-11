09d555e19355731baba3563608b972d644713514
Matt Chaput <matt@whoosh.ca>
2011-07-11 19:13:24 -0400

    Removed error flags.

--- src/whoosh/analysis.py
+++ src/whoosh/analysis.py
@@ -889,7 +889,7 @@ class PyStemmerFilter(StemFilter):
         :param cachesize: the maximum number of words to cache.
         """
         
-        import Stemmer
+        import Stemmer  #@UnresolvedImport
         
         stemmer = Stemmer.Stemmer(lang)
         stemmer.maxCacheSize = cachesize
@@ -901,7 +901,7 @@ class PyStemmerFilter(StemFilter):
         library.
         """
         
-        import Stemmer
+        import Stemmer  #@UnresolvedImport
         
         return Stemmer.algorithms()
     
