423e6aa0344d895ecbd9d8954b5cda46fef444ce
Matt Chaput <matt@whoosh.ca>
2011-07-21 23:48:35 -0400

    Added Python 3 to list of languages.

--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     "Natural Language :: English",
     "Operating System :: OS Independent",
     "Programming Language :: Python :: 2.5",
+    "Programming Language :: Python :: 3",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Text Processing :: Indexing",
     ],
