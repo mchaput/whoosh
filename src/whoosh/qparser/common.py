afba1c41da0cc9fe87eb13ba8d4d51c6eff82d69
Matt Chaput <matt@whoosh.ca>
2011-07-27 12:27:59 -0400

    Removed import of print_function for Python 2.5 compatibility. Fixes issue #160.

--- src/whoosh/qparser/common.py
+++ src/whoosh/qparser/common.py
@@ -30,8 +30,6 @@ This module contains common utility objects/functions for the other query
 parser modules.
 """
 
-from __future__ import print_function
-
 from whoosh.compat import string_type
 
 
