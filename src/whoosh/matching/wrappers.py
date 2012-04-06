8786b568d1fb9e6b8ce3c48a440ebf8e0bb7c651
Matt Chaput <matt@whoosh.ca>
2012-04-05 20:46:05 -0400

    Fixed reference to xrange on Python 3 (leftover from refactoring matching into a subpackage).

--- src/whoosh/matching/wrappers.py
+++ src/whoosh/matching/wrappers.py
@@ -25,6 +25,7 @@
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied, of Matt Chaput.
 
+from whoosh.compat import xrange
 from whoosh.matching import core
 
 
