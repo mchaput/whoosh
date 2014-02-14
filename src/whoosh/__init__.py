2d4c7378e9765f1e4056c32d8732fd90ecc6194a
Matt Chaput <matt@whoosh.ca>
2014-02-14 16:24:30 -0500

    Bumped version number to 2.5.7.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -25,7 +25,7 @@
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied, of Matt Chaput.
 
-__version__ = (2, 5, 6)
+__version__ = (2, 5, 7)
 
 
 def versionstring(build=True, extra=True):
