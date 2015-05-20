21027dc095519529e06492cfe9ba19a0171d0eb0
xtian muirhead <xtian@babbageclunk.com>
2015-05-20 13:50:10 +0100

    Change the version of my fork to be distinct from the base repo.
    This will mean that when we change the requirement back to the
    original repo dev VMs will update the library automatically.

--- src/whoosh/__init__.py
+++ src/whoosh/__init__.py
@@ -25,7 +25,7 @@
 # those of the authors and should not be interpreted as representing official
 # policies, either expressed or implied, of Matt Chaput.
 
-__version__ = (2, 7, 0)
+__version__ = (2, 7, 0, '-wilberforce')
 
 
 def versionstring(build=True, extra=True):
